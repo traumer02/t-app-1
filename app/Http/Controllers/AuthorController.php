53616e14927aad0e04d9d5ff14eb269f06b30c58
Kazybek Tattibekov <kazybektattibek@gmail.com>
2025-10-16 14:01:21 +0500

    add comment

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\Api\Author\StoreRequest;
 class AuthorController
 {
 
+    /**
+     * @param \App\Http\Requests\Api\Author\StoreRequest             $request
+     * @param \App\Contracts\Services\Http\Api\AuthorServiceContract $service
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(StoreRequest $request, AuthorServiceContract $service)
     {
         return $service->store($request);
